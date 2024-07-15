ad986ec1923f98ea90c6be858f2e35c5569d7d11
shyarnis <shyarnisgg88@gmail.com>
2024-07-15 21:56:48 +0545

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -3,7 +3,19 @@ import socket
 
 def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept()[0].sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+    client_socket, client_address = server_socket.accept()
+
+    request = client_socket.recv(1024).decode("utf-8")
+    request_data = request.split("\r\n")
+
+    if request_data[0].split(" ")[1] == "/":
+        # ["GET", "/", "HTTP/1.1"]
+        response = "HTTP/1.1 200 OK\r\n\r\n".encode()
+    else:
+        response = "HTTP/1.1 404 Not Found\r\n\r\n".encode()
+
+    client_socket.send(response)
+    client_socket.close()
 
 
 if __name__ == "__main__":
