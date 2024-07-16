febc12a090943f0237f8ca2df15570e6f87e7714
shyarnis <shyarnisgg88@gmail.com>
2024-07-16 12:01:29 +0545

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -7,10 +7,16 @@ def main():
 
     request = client_socket.recv(1024).decode("utf-8")
     request_data = request.split("\r\n")
+    path = request_data[0].split(" ")[1]
 
-    if request_data[0].split(" ")[1] == "/":
-        # ["GET", "/", "HTTP/1.1"]
+    if path == "/":
         response = "HTTP/1.1 200 OK\r\n\r\n".encode()
+
+    elif path.startswith("/echo/"):
+        string: str = path.split("/")[-1]
+        # ['', 'echo', 'hello']
+        response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(string)}\r\n\r\n{string}".encode()
+
     else:
         response = "HTTP/1.1 404 Not Found\r\n\r\n".encode()
 
