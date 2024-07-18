66c58ebf214408c63c0ba7f13a32252377c54277
shyarnis <shyarnisgg88@gmail.com>
2024-07-18 18:58:37 +0545

    refactor: use of typing

--- app/main.py
+++ app/main.py
@@ -5,32 +5,34 @@ import os
 import gzip
 
 
-def client_request(client_socket):
-    request = client_socket.recv(1024).decode("utf-8")
-    request_data = request.split("\r\n")
-    path = request_data[0].split(" ")[1]
-    body = request_data[-1]
-    request_method = request_data[0].split(" ")[0]
+def client_request(client_socket: socket.socket) -> None:
+    request: str = client_socket.recv(1024).decode("utf-8")
+    request_data: list[str] = request.split("\r\n")
+    path: str = request_data[0].split(" ")[1]
+    body: str = request_data[-1]
+    request_method: str = request_data[0].split(" ")[0]
 
     if request_method == "GET":
-        response = get_request_method(path, request_data)
+        response: bytes = get_request_method(path, request_data)
 
-    if request_method == "POST":
-        response = post_request_method(path, body)
+    elif request_method == "POST":
+        response: bytes = post_request_method(path, body)
+
+    else:
+        response: bytes = f"HTTP/1.1 405 Method Not Allowed\r\n\r\n".encode()
 
     client_socket.send(response)
     client_socket.close()
 
 
-def get_request_method(path, request_data):
+def get_request_method(path: str, request_data: list[str]) -> bytes:
     if path == "/":
         return f"HTTP/1.1 200 OK\r\n\r\n".encode()
 
     elif path.startswith("/echo/"):
         string: str = path.split("/")[-1]
         # ['', 'echo', 'hello']
-
-        compression_scheme = ""
+        compression_scheme: str = ""
 
         # find comppresion_scehme from header.
         for header in request_data:
@@ -41,30 +43,45 @@ def get_request_method(path, request_data):
                     break
 
         if compression_scheme == "gzip":
-            compressed_content = gzip.compress(string.encode())
+            compressed_content: bytes = gzip.compress(string.encode())
             return (
-                f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Encoding: gzip\r\nContent-Length: {len(compressed_content)}\r\n\r\n".encode()
+                f"HTTP/1.1 200 OK\r\n"
+                f"Content-Type: text/plain\r\n"
+                f"Content-Encoding: gzip\r\n"
+                f"Content-Length: {len(compressed_content)}\r\n\r\n".encode()
                 + compressed_content
             )
 
         else:
-            return f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(string)}\r\n\r\n{string}".encode()
+            return (
+                f"HTTP/1.1 200 OK\r\n"
+                f"Content-Type: text/plain\r\n"
+                f"Content-Length: {len(string)}\r\n\r\n{string}".encode()
+            )
 
     elif path.startswith("/user-agent"):
-        user_agent = request_data[2].split(": ")[1]
+        user_agent: str = request_data[2].split(": ")[1]
         # ['GET /user-agent HTTP/1.1', 'Host: localhost:4221', 'Accept: */*', 'User-Agent: foobar/1.2.3', '', '']
         # ['Accept', '*/*']
         # */*
-        return f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(user_agent)}\r\n\r\n{user_agent}".encode()
+        return (
+            f"HTTP/1.1 200 OK\r\n"
+            f"Content-Type: text/plain\r\n"
+            f"Content-Length: {len(user_agent)}\r\n\r\n{user_agent}".encode()
+        )
 
     elif path.startswith("/files"):
-        directory = sys.argv[2]
-        filename = path.split("/")[-1]
+        directory: str = sys.argv[2]
+        filename: str = path.split("/")[-1]
 
         try:
             with open(f"/{directory}/{filename}", "r") as file:
-                response_body = file.read()
-                return f"HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {len(response_body)}\r\n\r\n{response_body}".encode()
+                response_body: str = file.read()
+                return (
+                    f"HTTP/1.1 200 OK\r\n"
+                    f"Content-Type: application/octet-stream\r\n"
+                    f"Content-Length: {len(response_body)}\r\n\r\n{response_body}".encode()
+                )
 
         except FileNotFoundError as e:
             return f"HTTP/1.1 404 Not Found\r\n\r\n".encode()
@@ -72,14 +89,12 @@ def get_request_method(path, request_data):
     else:
         return "HTTP/1.1 404 Not Found\r\n\r\n".encode()
 
-    return
 
-
-def post_request_method(path, body):
+def post_request_method(path: str, body: str) -> bytes:
     if path.startswith("/files"):
-        directory = sys.argv[2]
-        filename = path.split("/")[-1]
-        file_path = os.path.join(directory, filename)
+        directory: str = sys.argv[2]
+        filename: str = path.split("/")[-1]
+        file_path: str = os.path.join(directory, filename)
 
         try:
             # with open(f"/{directory}/{filename}", "w") as file:
@@ -90,11 +105,13 @@ def post_request_method(path, body):
         except FileNotFoundError as e:
             return f"HTTP/1.1 404 Not Found\r\n\r\n".encode()
 
-    return
+    return f"HTTP/1.1 400 Bad Request\r\n\r\n".encode()
 
 
-def main():
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+def main() -> None:
+    server_socket: socket.socket = socket.create_server(
+        ("localhost", 4221), reuse_port=True
+    )
 
     with concurrent.futures.ThreadPoolExecutor() as executor:
 
